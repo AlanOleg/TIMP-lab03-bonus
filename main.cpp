37701a3783eee702823792937acc38192655bd5f
Alan <57073942+AlanOleg@users.noreply.github.com>
2024-05-17 17:54:49 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -5,6 +5,6 @@ int main(){
   int count;
   cin >> count;
   for (int i = 0; i < count; i++){
-    cout << "Maybe I will not miss the next deadline" << endl;
+    cout << "Maybe I will not miss the next deadline" << endl; // I already missed it ¯⁠\⁠_⁠(⁠ツ⁠)⁠_⁠/⁠¯
   }
 }
