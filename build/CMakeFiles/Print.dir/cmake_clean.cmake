cc410cdf0b588ee3aeab6fb6aec68089ece2f18a
AlanOleg <oleg.ru.6@gmail.com>
2024-05-26 15:28:07 +0300

    added build folder

---
+++ build/CMakeFiles/Print.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/Print.dir/main.cpp.o"
+  "CMakeFiles/Print.dir/main.cpp.o.d"
+  "Print"
+  "Print.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Print.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
